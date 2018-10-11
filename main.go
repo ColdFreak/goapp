4dd16cd1f27a62db9c1cb0b9a491c2108e011099
ColdFreak <madfrogme@gmail.com>
2018-10-11 12:47:05 +0900

    ファイル修正

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Hi")
-	fmt.Fprintln(w, "Hello World from Wang")
+	fmt.Fprintln(w, "Hello World by Wang")
 }
 
 func main() {
